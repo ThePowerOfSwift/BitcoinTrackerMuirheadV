b82d65aaa7aef28c9b9348c09ff5ec42f3ed64d4
Chad-Michael <c85529@wicmb0193.internal.cigna.com>
2018-11-08 15:52:03 -0500

    test on home mac

--- BitcoinTrackerMuirheadV/ViewController.swift
+++ BitcoinTrackerMuirheadV/ViewController.swift
@@ -7,15 +7,12 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController,UIPickerViewDelegate, UIPickerViewDataSource {
+class ViewController: UIViewController{
     
-    
-    
-    
-
-    
-    
+    @IBOutlet weak var priceLabel: UILabel!
     let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
@@ -25,13 +22,60 @@ class ViewController: UIViewController,UIPickerViewDelegate, UIPickerViewDataSou
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //priceLabel.translatesAutoresizingMaskIntoConstraints = false
     }
+    
+    
+    func getBitcoinData(apiUrl: URL){
+        print(apiUrl)
+        
+        //make api request
+        Alamofire.request(apiUrl,method: .get).responseJSON { response in
+            print(response)
+            //response is the data gotten back after api call
+            if response.result.isSuccess{
+                
+                //convert response data from ANY format to JSON format by casting
+                let bitcoinJSON: JSON = JSON(response.result.value!)
+                
+                self.updateBitcoinData(bitcoinJSON: bitcoinJSON)
+                
+                
+            }else if response.result.isFailure{
+                print("Failed to get API Data sorry..")
+                self.priceLabel.text = "Jahko"
+            }
+            else{
+                print("ok")
+            }
+        }
+    }
+    
+    func updateBitcoinData(bitcoinJSON:JSON){
+        
+        if let bitcoinPrice = bitcoinJSON["ask"].double
+        {
+            priceLabel.text = String(bitcoinPrice)
+            
+        }
+        else
+        {
+            print("failed in updating bitcoinData")
+        }
+    }
+    
+    
+}
+
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
     
     //number of columns
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-
+    
     //number of rows
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return currencyArray.count
@@ -45,7 +89,9 @@ class ViewController: UIViewController,UIPickerViewDelegate, UIPickerViewDataSou
     //which row user selected
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         finalURL = baseURL + currencyArray[row]
-        print(finalURL)
+        var finalURL2 = URL(string: finalURL)
+        getBitcoinData(apiUrl: finalURL2!)
     }
+    
 }
 
